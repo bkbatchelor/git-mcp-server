45c615eb352c9ebda6d38834e5d488b700074ec4
bkbatchelor <brian.batchelor@sandboxdev.io>
2025-12-20 21:29:46 -0500

    pre-revert commit

---
+++ build/resources/main/prompts/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * External prompt templates for AI interactions.
+ * 
+ * This directory contains externalized prompt templates in .st or .txt format
+ * for use with Spring AI's prompt templating system.
+ */
