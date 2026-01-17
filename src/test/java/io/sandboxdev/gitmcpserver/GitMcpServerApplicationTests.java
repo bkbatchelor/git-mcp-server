ec0a99066217cb1759dab7c1ab5ac2b9d68723c6
bkbatchelor <brian.batchelor@sandboxdev.io>
2026-01-16 22:26:11 -0500

    chore: initializing project

---
+++ src/test/java/io/sandboxdev/gitmcpserver/GitMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sandboxdev.gitmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
